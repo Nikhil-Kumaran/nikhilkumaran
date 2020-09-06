76eea287d4b5ed7843ddc4fb270f02b9ecd5c279
Nikhil Kumaran <nikhil.kumaran.2011@gmail.com>
2020-09-06 19:37:25 +0530

    📦 Add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,32 @@
+import { useEffect } from "react";
 import { MDXProvider } from "@mdx-js/react";
 import MDXComponents from "../components/MDXComponents";
-import Router from "next/router";
+import { useRouter } from "next/router";
 import NProgress from "nprogress";
 import "../styles/reset.css";
 import "nprogress/nprogress.css";
 import "../styles/global.css";
-
-Router.events.on("routeChangeStart", () => NProgress.start());
-Router.events.on("routeChangeComplete", () => NProgress.done());
-Router.events.on("routeChangeError", () => NProgress.done());
+import * as gtag from "../utils/gtag";
 
 const App = ({ Component, pageProps }) => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteComplete = (url) => {
+      NProgress.done();
+      gtag.pageview(url);
+    };
+
+    router.events.on("routeChangeStart", () => NProgress.start());
+    router.events.on("routeChangeError", () => NProgress.done());
+    router.events.on("routeChangeComplete", handleRouteComplete);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteComplete);
+      router.events.off("routeChangeStart", () => NProgress.start());
+      router.events.off("routeChangeError", () => NProgress.done());
+    };
+  }, [router.events]);
+
   return (
     <MDXProvider components={MDXComponents}>
       <Component {...pageProps} />
